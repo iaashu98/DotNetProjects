8a7e3aeb8f95ba3231f97ea68e0b527c79f9f267
iaashu98 <ashutoshsamraj1@gmail.com>
2024-02-16 19:39:14 +0530

    First Commit

---
+++ Projects/SolutionApp/Solution/Model/CalculatorModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Solution.Model
+{
+    public class CalculatorModel
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+
+}
+
---
+++ myProject/program1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverCode
+{
+
+    class GFG
+    {
+        static void Main(string[] args)
+        {
+            //string s = "i.like.this.program.very.much";
+                
+            // Solution obj = new Solution();
+            // string res = obj.reverseWords(s);
+            // Console.Write(res);
+            // Console.Write("\n");
+
+            //string a = "geeksforgeeks";
+            //string b = "forgeeksgeeks"; // 1, 6, 2, 8, 5, 6
+            int[] ints= new int[] { 1,6,2,8,5, 16, 3, 15, 18};
+            Solution obj = new Solution();
+
+            //foreach (int var in obj.dupArray(ints)){
+                Console.WriteLine(obj.dupFind(ints));
+           // }
+
+            //obj.runningSum(ints);
+            //int res = obj.isAnagram(a,b);
+            // if(res==0){
+            //     Console.Write("YES");
+            // }
+            // else{
+            //     Console.Write("NO");    
+            // }
+            // Console.Write("\n");
+        }
+    }
+    
+}
+
+class Solution
+{
+    //Complete this function
+    public string reverseWords(string s)
+    {
+    string[] split = s.Split('.');
+
+        Array.Reverse(split);
+        string reversedString = string.Join('.', split);
+        return reversedString;
+    }
+
+    public int isAnagram(string a, string b)
+    {
+        char[] charArray = a.ToCharArray();
+        char[] charArray2 = b.ToCharArray();
+
+        Array.Sort(charArray); // Sort the characters in the charArray in ascending order
+        Array.Sort(charArray2);
+
+        string sortedString = new string(charArray);
+        string sortedString2 = new string(charArray2);
+        int res = sortedString.CompareTo(sortedString2);
+        return res;
+    }
+
+    public void runningSum(int[] arr){
+        int rSum =0;
+        for(int i=0;i<arr.Length; i++){
+            rSum += arr[i];
+            arr[i]=rSum;
+        }
+        for(int i=0;i<arr.Length; i++){
+                Console.WriteLine(arr[i]);
+        }
+    }
+
+    public int dupFind(int[] arr){
+        HashSet<int> ht = new HashSet<int>();
+        //bool flag=false;
+        int number=0;
+        for(int i=0; i< arr.Length; i++){
+            if(ht.Contains(arr[i])){
+               // flag=true;
+                number=arr[i];
+                break;
+            }
+            else{
+                ht.Add(arr[i]);
+            }
+        }
+        return number;
+    }
+
+    public int dupArray(int[] arr){
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        List<int> dupLi = new List<int>();
+        List<int> rahul = new List<int>();
+        int a=0;
+        for(int i=0; i < arr.Length; i++){
+            if(dict.ContainsKey(arr[i])){
+                //dupLi.Add(arr[i]);
+                a=arr[i];
+                break;
+            }
+            else{
+                rahul.Add(arr[i]);
+                dict.Add(arr[i], i);
+            }
+        }
+        foreach(int var in rahul){
+            Console.WriteLine(var);
+        }
+        Console.WriteLine("________________");
+        return a;
+    }
+
+
+}
+
